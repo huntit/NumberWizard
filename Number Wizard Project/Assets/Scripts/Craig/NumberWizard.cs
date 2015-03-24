218fcf42adc2ad6e2149a0f42438b5da0eaf0781
Craig Hargraves <cs_hargraves@yahoo.com>
2015-03-24 14:29:18 +1000

    Craig's completed Number Wizard

--- Number Wizard Project/Assets/Scripts/Craig/NumberWizard.cs
+++ Number Wizard Project/Assets/Scripts/Craig/NumberWizard.cs
@@ -3,27 +3,57 @@ using System.Collections;
 
 public class NumberWizard : MonoBehaviour {
 
+	int max;
+	int min;
+	int guess;
+
 	// Use this for initialization
 	void Start() 
 	{
-		print("Welcome to Number Wizard");
-		print("Pick a number in your head, but don't tell me !");
-
-		int max = 1000;
-		int min = 1;
-		print ("The highest number you can pick is " + max + ".");
-		print ("The lowest number you can pick is " + min + ".");
-		print ("Choose a number between " + min + " and " + max + ".");
-		print ("Good luck !!");
-		print ("May lady luck (and logic) have mercy on your soul...");
-		print ("This is from the Peter branch");
-		print ("This is now in the Craig branch");
+		StartGame();
 
 	}
-	
+
+	void StartGame()
+	{
+		// StartGame prints the introductory text for the game.
+		max = 1001;
+		min = 1;
+		guess = 500;
+
+		print ("========================");
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me !");
+		
+		print ("The highest number you can pick is 1000.");
+		print ("The lowest number you can pick is " + min + ".");
+		
+		print ("Is the number higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+	}
+
+	void NextGuess()
+	{
+		// NextGuess calculates the next guess as the midpoint between the current min and max values.
+		guess = (max + min) / 2;
+		print ("Higher or lower than " + guess + "?");
+		print ("Up = higher, down = lower, return = equal");
+	}
 	// Update is called once per frame
 	void Update() 
 	{
-	
+		if (Input.GetKeyDown ("up")) {
+			min = guess;
+			NextGuess ();
+
+		} else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			max = guess;
+			NextGuess ();
+
+		} else if (Input.GetKeyDown (KeyCode.Return)) {
+			print ("I won!");
+			StartGame ();
+
+		}
 	}
 }
